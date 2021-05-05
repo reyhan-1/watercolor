22e2f137c29c2bd08ffea884756162ee62563ea0
reyhan-1 <54553618+reyhan-1@users.noreply.github.com>
2021-05-05 18:13:05 +0300

    navbar updates

---
+++ src/comps/Home.js
@@ -0,0 +1,31 @@
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   Link
+ } from "react-router-dom";
+ import React, {useState} from 'react';
+ import ImageGrid from './imageGrid';
+ import Modal from './Modal';
+ import Title from './Title';
+ import UploadForm from './UploadForm';
+import About from "./About";
+ 
+ const Home = () => {
+   const [selectedImg, setSelectedImg] = useState(null);
+ 
+   
+   return (    
+      
+     <div className="App" >
+        <UploadForm/>
+       <ImageGrid setSelectedImg= {setSelectedImg}/>
+       {selectedImg && <Modal selectedImg={selectedImg} 
+       setSelectedImg={setSelectedImg}/> }
+ 
+     </div>
+  
+   );
+ }
+ 
+ export default Home;
